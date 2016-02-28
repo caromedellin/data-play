c4fd7b184ba0bfa2ef1adbc5fa10bb4794e7a348
caromedellin <caromedellin@gmail.com>
2016-02-28 18:25:07 -0500

    set a couple of working examples for numpy

--- sample.py
+++ sample.py
@@ -1,4 +1,12 @@
 import scipy
-import numpy
+import numpy as np
 
-firstArray = [[1,2,3],[1,3,4],[0,0,0]]
+a = np.array([[1.0, 2.0], [3.0, 4.0]])
+
+# this calls the sub package of  the numpy lib nad makes this in to the inverse array
+
+np.linalg.inv(a)
+
+# producto de matrices, but it's Ø of nˆ2, if you are given some input 
+# length you need at least twice as much time in the worse case scenario
+np.dot (a, a)
